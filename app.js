c1572792da7f9b11a2bec28e095688c31c8c6957
Ahmed Mohamed <alamerahmed00@gmail.com>
2026-02-07 16:52:09 +0300

    init

---
+++ app.js
@@ -0,0 +1,45 @@
+require('dotenv').config();
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const logger = require('morgan');
+
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
+const dronesRouter = require('./routes/drones');
+const adminsRouter = require('./routes/admins');
+
+const app = express();
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+app.use('/drones', dronesRouter);
+app.use('/admins', adminsRouter);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.json(
+    {
+      status: 404,
+      message: 'resource is not founded'
+    }
+  );
+});
+
+module.exports = app;
