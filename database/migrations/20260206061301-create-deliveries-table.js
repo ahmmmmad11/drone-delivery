ce62d91581eacba871a96505f047fa8b4ab5557d
Ahmed Mohamed <alamerahmed00@gmail.com>
2026-02-07 19:55:52 +0300

    fix: make location col nullable in deliveris table

--- database/migrations/20260206061301-create-deliveries-table.js
+++ database/migrations/20260206061301-create-deliveries-table.js
@@ -23,7 +23,7 @@ module.exports = {
         type: Sequelize.STRING
       },
       location: {
-        allowNull: false,
+        allowNull: true,
         type: Sequelize.JSON
       },
       createdAt: {
